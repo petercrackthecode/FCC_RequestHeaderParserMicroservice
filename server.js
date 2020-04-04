d6603cb42a1b8ee7af71f5426ef37b63d6561611
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-04-03 19:51:07 -0700

    Nothing much

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-let express = require("express");
-let app = express();
-require('dotenv/config');
+const express = require("express");
+const app = express();
+const path = require("path");
+require("dotenv/config");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-let cors = require("cors");
+const cors = require("cors");
 app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-app.use(express.static(path.join(__dirname, '/static/style')));
+app.use(express.static(path.join(__dirname, "/static/style")));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/static/index.html");
+  res.sendFile(__dirname + "/static/index.html");
 });
 
 app.get("/api/hello", (req, res) => {
-    res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/whoami", (req, res) => {
+  res.json({sayHello: "Hello"});
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Your app is listening on port ${process.env.PORT}`);
+  console.log(`Your app is listening on port ${process.env.PORT}`);
 });
